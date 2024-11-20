251e68b3c72db47ed920953d681657528e05574e
jbesoomi <jbesoomi@gmail.com>
2024-11-20 14:14:21 -0800

    remove unused variables

--- lib/passkit/notification.rb
+++ lib/passkit/notification.rb
@@ -9,15 +9,10 @@ module Passkit
       connection = Apnotic::Connection.new(cert_path: Rails.root.join(Passkit.configuration.private_p12_certificate),
                                            cert_pass: Passkit.configuration.certificate_key)
 
-      responses_headers = {}
       passes.each do |pass|
-        pass_key = "pass_#{pass.id}"
-        responses_headers[pass_key] = {}
-
         pass.touch # update the updated_at field to mark it as changed
 
         pass.devices.each do |device|
-          device_key = "device_#{device.id}"
           notification = Apnotic::Notification.new(device.push_token)
           notification.alert = {} # empty json as required in apple docs
           notification.topic = Passkit.configuration.pass_type_identifier
